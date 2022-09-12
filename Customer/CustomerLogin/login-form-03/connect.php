f8080105eae2340207b253d575d25aa02bbee357
Amirulhani02 <amirulhani02@gmail.com>
2022-09-13 05:30:44 +0800

    Register + Login

---
+++ Customer/CustomerLogin/login-form-03/connect.php
@@ -0,0 +1,16 @@
+<?php
+$hostname = "localhost"; $user = "root"; $pass = ""; $db = "zooproject";
+
+//create/open connection
+$conn = mysqli_connect($hostname,$user,$pass,$db);
+
+//check connection
+if($conn){
+	echo "connection successful";
+}
+else{
+	die(mysqli_error($conn));
+}
+
+
+?>
